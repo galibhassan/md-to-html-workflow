28b54ddccda0b890e5dd7f4f6395f9c6c528077f
Galib Hassan <galib.hassan.0@gmail.com>
2020-04-07 04:12:23 +0200

    cleaning up

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const path = require("path");
 const fs = require("fs");
-const showdown = require("showdown");
-const converter = new showdown.Converter({ tables: true });
+var replace = require("replace");
 const makeDocStruct = require("./docStruct");
+const showdown = require("showdown");
+
+const converter = new showdown.Converter({ tables: true });
 
 const userInputPath = process.argv[2];
 const rootDir = path.dirname(__dirname);
 const inputFilePath = path.resolve(path.join(rootDir, userInputPath));
 
-var replace = require("replace");
-
-// const sample_md = fs.readFileSync(path.resolve(__dirname, "../", "sample", "readme.md"), { encoding: "utf8" });
 const sample_md = fs.readFileSync(inputFilePath, { encoding: "utf8" });
 
 const outputHTMLChunk = converter.makeHtml(sample_md);
-// let regexp = RegExp("^&.*;$", "g");
 
 const docStructDOM = makeDocStruct(outputHTMLChunk);
 const header = fs.readFileSync(path.resolve(__dirname, "../", "template", "header.txt"), { encoding: "utf8" });
